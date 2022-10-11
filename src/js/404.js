1e2d1017881f8b8143567856802e1917ea52572e
naybiblu <91787637+naybiblu@users.noreply.github.com>
2022-10-11 21:55:27 +0800

    Update 404.js

--- src/js/404.js
+++ src/js/404.js
@@ -1,13 +1,26 @@
 var trigger = document.getElementById("gif");
 var bStyle = document.body.style;
+var desc = document.getElementById("desc");
 var audio = new Audio("./../../resources/easter-eggs/easter-egg-1.mp3");
 
 trigger.addEventListener("click", async () => {
   if (bStyle.backgroundColor === "") {
     bStyle.backgroundColor = "#00008b"
-    audio.play(); 
+    await audio.play();
   } else {
     bStyle.backgroundColor = "";
-    audio.load();
+    await audio.load();
   }
+
+  var interval = setInterval(async() => {
+    if (audio.currentTime >= 10) {
+      desc.innerHTML = "Eh, paano kung...";
+    }
+
+    if (audio.ended === true) {
+      bStyle.backgroundColor = "";
+      desc.innerHTML = "It's not magic.<br>Just an <ins>imaginary page</ins>.";
+      clearInterval(interval);
+    }
+  }, 100);
 });
