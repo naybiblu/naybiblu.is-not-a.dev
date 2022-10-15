e1eca1bfc7542e3473995ae7c522ec357bb68c1c
naybiblu <91787637+naybiblu@users.noreply.github.com>
2022-10-16 02:57:11 +0800

    Update shout-out.js

--- src/js/shout-out.js
+++ src/js/shout-out.js
@@ -3,16 +3,15 @@ const msg = document.getElementById("message");
       
 send.addEventListener("click", async () => {
   if (!msg.value) return alert("MESSAGE! GIVE ME A MESSAGE!!!");
-  fetch("https://naypi.is-not-a.dev/api/e-mail", {
+  const res = await fetch("https://naypi.is-not-a.dev/api/e-mail", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    mode: 'no-cors', 
+    //mode: 'no-cors', 
     body: JSON.stringify({ content: msg.value })
-  }).catch(err => {
-  if (err) return alert("Oh, no! A wild error appeared! Please use evade move. " + err);
+  });
+  if (!res.ok) return alert("Oh, no! A wild error appeared! Please use evade move.");
   alert("Nice one! The message has been sent!");
   window.location.href = "https://naybiblu.is-not-a.dev";
-  });
 });
