f403e72135a98f7d856f7d608264c2a5f0dcef02
naybiblu <91787637+naybiblu@users.noreply.github.com>
2022-10-16 03:09:24 +0800

    Update shout-out.js

--- src/js/shout-out.js
+++ src/js/shout-out.js
@@ -10,8 +10,12 @@ send.addEventListener("click", async () => {
     },
     //mode: 'no-cors', 
     body: JSON.stringify({ content: msg.value })
+  }).catch(err => {
+    if (err) {
+      return alert("Oh, no! A wild error appeared! Please use evade move. " + err);
+    } else {
+      alert("Nice one! The message has been sent!");
+      window.location.href = "https://naybiblu.is-not-a.dev";
+    }
   });
-  if (res.status !== 269) return alert("Oh, no! A wild error appeared! Please use evade move.");
-  alert("Nice one! The message has been sent!");
-  window.location.href = "https://naybiblu.is-not-a.dev";
 });
